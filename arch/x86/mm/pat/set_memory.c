f36e8cf736171185e5f2f7ed69f7f7e18237d7a3
Dave Hansen <dave.hansen@linux.intel.com>
2024-01-23 08:36:23 -0800

    x86/mm: Simplify redundant overlap calculation
    There have been a couple of reports that the two sides of the
    overlaps() calculation are redundant.  I spent way too much time
    looking at this, but I became convinced that they are redundant
    when a little test program of mine produced identical disassembly
    for both versions of the check.
    
    Remove the second condition.  It is exactly the same as the first.
    
    Fixes: 91ee8f5c1f50 ("x86/mm/cpa: Allow range check for static protections")
    Reported-by: David Binderman <dcb314@hotmail.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: x86@kernel.org
    Reviewed-by: Sohil Mehta <sohil.mehta@intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>

--- arch/x86/mm/pat/set_memory.c
+++ arch/x86/mm/pat/set_memory.c
@@ -484,8 +484,7 @@ collapse_large_pages:
 static bool overlaps(unsigned long r1_start, unsigned long r1_end,
 		     unsigned long r2_start, unsigned long r2_end)
 {
-	return (r1_start <= r2_end && r1_end >= r2_start) ||
-		(r2_start <= r1_end && r2_end >= r1_start);
+	return (r1_start <= r2_end && r1_end >= r2_start);
 }
 
 #ifdef CONFIG_PCI_BIOS
