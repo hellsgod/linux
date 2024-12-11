618e84adc5551cd5267dfd961ee621b38d17ae33
K Prateek Nayak <kprateek.nayak@amd.com>
2024-12-11 18:55:50 +0000

    sched/fair: Do not compute overloaded status unnecessarily during lb
    Only set sg_overloaded when computing sg_lb_stats() at the highest sched
    domain since rd->overloaded status is updated only when load balancing
    at the highest domain. While at it, move setting of sg_overloaded below
    idle_cpu() check since an idle CPU can never be overloaded.
    
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10331,9 +10331,6 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		nr_running = rq->nr_running;
 		sgs->sum_nr_running += nr_running;
 
-		if (nr_running > 1)
-			*sg_overloaded = 1;
-
 		if (cpu_overutilized(i))
 			*sg_overutilized = 1;
 
@@ -10345,6 +10342,10 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 			/* Idle cpu can't have misfit task */
 			continue;
 		}
+
+		/* Overload indicator is only updated at root domain */
+		if (!env->sd->parent && nr_running > 1)
+			*sg_overloaded = 1;
 
 #ifdef CONFIG_NUMA_BALANCING
 		/* Only fbq_classify_group() uses this to classify NUMA groups */
